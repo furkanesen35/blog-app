3d50358bb747fe83631fb231fe6c52821c71532e
Furkan Esen <furkanesen35@gmail.com>
2024-02-14 18:11:34 +0100

    authentication fully done

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth import authenticate,login
+from .forms import UserForm,UserProfileForm
 
 def home(request):
  return HttpResponse("welcome")
 
 def register(request):
- form = UserCreationForm()
+ form_user = UserForm()
+ form_profile = UserProfileForm()
  if request.method == "POST":
-  form = UserCreationForm(request.POST)
-  if form.is_valid():
-   form.save()
+  form_user = UserForm(request.POST)
+  form_profile = UserProfileForm(request.POST,request.FILES)
+  if form_user.is_valid():
+   user = form_user.save()
+   profile = form_profile.save(commit=False)
+   profile.user = user
+   profile.save()
+   login(request,user)
    return redirect("home")
  context = {
-  "form": form,
+  'form_user': form_user,
+  'form_profile': form_profile,
  }
  return render(request, 'users/register.html', context)
 
