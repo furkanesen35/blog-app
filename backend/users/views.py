018dadc2517e4afc3a089cba974d400b01e059a7
Furkan Esen <furkanesen35@gmail.com>
2024-02-14 17:20:51 +0100

    register fully operational

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import authenticate,login
 
 def home(request):
  return HttpResponse("welcome")
+
+def register(request):
+ form = UserCreationForm()
+ if request.method == "POST":
+  form = UserCreationForm(request.POST)
+  if form.is_valid():
+   form.save()
+   return redirect("home")
+ context = {
+  "form": form,
+ }
+ return render(request, 'users/register.html', context)
