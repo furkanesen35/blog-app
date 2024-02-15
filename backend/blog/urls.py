c6c1efe9cf83604048bdd13ab8cb94a76624f943
Furkan Esen <furkanesen35@gmail.com>
2024-02-15 15:18:36 +0100

    add done

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home,add
 
 urlpatterns = [
  path('', home, name="home"),
+ path('add/', add, name="add"),
 ]
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from .models import Post,Comment,PostView,Like
+from .forms import PostForm,CommentForm
 
 def home(request):
- return HttpResponse("welcome")
+ posts = Post.objects.filter(status="p")
+ context = {
+  "posts": posts,
+ }
+ return render(request, "blog/blogs.html", context)
 
-# def add(request):
+def add(request):
+ form = PostForm()
+ if request.method == "POST":
+  form = PostForm(request.POST)
+  if form.is_valid():
+   post = form.save(commit=False)
+   post.author = request.user
+   post.save()
+   return redirect("home")
+ context = {
+  "form" : form
+ }
+ return render(request, "blog/add.html", context)
