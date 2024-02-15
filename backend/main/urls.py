a00258c10d5036e2104931da197ae6752fd37a16
Furkan Esen <furkanesen35@gmail.com>
2024-02-15 11:59:27 +0100

    blog created

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 
 urlpatterns = [
  path('admin/', admin.site.urls),
- path('', include("users.urls")),
+ path('account/', include("users.urls")),
+ path('', include("blog.urls")),
 ]
