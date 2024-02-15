bf986b66ae5e94d657aa49168a9591700ce526dc
Furkan Esen <furkanesen35@gmail.com>
2024-02-15 13:57:51 +0100

    other models created

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -18,7 +18,7 @@ class Post(models.Model):
  )
  title = models.CharField(max_length=100)
  content = models.TextField()
- image = models.ImageField(upload_to=user_directory_path,default='django.jpg')
+ image = models.ImageField(upload_to=user_directory_path)
  category = models.ForeignKey(Category, on_delete=models.PROTECT, blank=True, null=True)
  publish_date = models.DateTimeField(auto_now_add=True)
  update_date = models.DateTimeField(auto_now=True)
@@ -27,3 +27,24 @@ class Post(models.Model):
  slug = models.SlugField(blank=True)
  def __str__(self):
   return self.title
+ 
+class Comment(models.Model):
+ user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment")
+ post = models.ForeignKey(Post, on_delete=models.CASCADE)
+ time_stamp = models.DateTimeField(auto_now_add=True)
+ content = models.TextField()
+ def __str__(self):
+  return self.user.username
+ 
+class Like(models.Model):
+ user = models.ForeignKey(User, on_delete=models.CASCADE)
+ post = models.ForeignKey(Post, on_delete=models.CASCADE)
+ def __str__(self):
+  return self.user.username
+ 
+class PostView(models.Model):
+ user = models.ForeignKey(User, on_delete=models.CASCADE)
+ post = models.ForeignKey(Post, on_delete=models.CASCADE)
+ time_stamp = models.DateTimeField(auto_now_add=True)
+ def __str__(self):
+  return self.user.username
