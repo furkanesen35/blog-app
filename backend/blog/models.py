3d06123e5efb625e34203fcb9d83704bb72f9140
Furkan Esen <furkanesen35@gmail.com>
2024-02-15 13:30:33 +0100

    blog model done

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,1 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+def user_directory_path(instance, filename):
+ return 'blog/{0}/{1}'.format(instance.author.id)
+
+class Category(models.Model):
+ name = models.CharField(max_length=100)
+ class Meta:
+  verbose_name_plural = "Categories"
+ def __str__(self):
+  return self.name
+
+class Post(models.Model):
+ OPTIONS = (
+  ('d','Draft'),
+  ('p','Published'),
+ )
+ title = models.CharField(max_length=100)
+ content = models.TextField()
+ image = models.ImageField(upload_to=user_directory_path,default='django.jpg')
+ category = models.ForeignKey(Category, on_delete=models.PROTECT, blank=True, null=True)
+ publish_date = models.DateTimeField(auto_now_add=True)
+ update_date = models.DateTimeField(auto_now=True)
+ author = models.ForeignKey(User, on_delete=models.CASCADE)
+ status = models.CharField(max_length=10, choices=OPTIONS, default='d')
+ slug = models.SlugField(blank=True)
+ def __str__(self):
+  return self.title
