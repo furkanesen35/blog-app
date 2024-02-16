2fb40cf31707056d940aae30d7c8595ee2db33ca
Furkan Esen <furkanesen35@gmail.com>
2024-02-16 12:20:02 +0100

    cmments done

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home,add,update,delete
+from .views import home,add,update,delete,details
 
 urlpatterns = [
  path('', home, name="home"),
  path('add/', add, name="add"),
  path('<str:slug>/update', update, name="update"),
  path('<str:slug>/delete', delete, name="delete"),
+ path('<str:slug>/details', details, name="details"),
 ]
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from .models import Post,Comment,PostView,Like
 from .forms import PostForm,CommentForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 def home(request):
  posts = Post.objects.filter(status="p")
@@ -11,20 +12,42 @@ def home(request):
  }
  return render(request, "blog/blogs.html", context)
 
+@login_required()
 def add(request):
  form = PostForm()
  if request.method == "POST":
-  form = PostForm(request.POST)
+  form = PostForm(request.POST, request.FILES)
   if form.is_valid():
    post = form.save(commit=False)
    post.author = request.user
    post.save()
+   messages.success(request, "Post created successfully!")
    return redirect("home")
  context = {
   "form" : form
  }
  return render(request, "blog/add.html", context)
 
+def details(request,slug):
+ form = CommentForm()
+ obj = get_object_or_404(Post, slug=slug)
+ if request.user.is_authenticated:
+  PostView.objects.get_or_create(user=request.user, post=obj)
+ if request.method == "POST":
+  form = CommentForm(request.POST)
+  if form.is_valid():
+   comment = form.save(commit=False)
+   comment.user = request.user
+   comment.post = obj
+   comment.save()
+   return redirect("details", slug=slug)
+ context = {
+  'object': obj,
+  'form': form,
+ } 
+ return render(request, 'blog/details.html', context)
+
+@login_required()
 def update(request,slug):
  post = get_object_or_404(Post, slug=slug)
  form = PostForm(request.POST or None, request.FILES or None, instance=post)
@@ -50,3 +73,15 @@ def delete(request,slug):
   'post': post,
  }
  return render(request, 'blog/delete.html', context)
+
+@login_required()
+def like(request, slug):
+ if request.method == "POST":
+  obj = get_object_or_404(Post, slug=slug)
+  like_qs = Like.objects.filter(user=request.user, post=obj)
+  if like_qs.exists():
+   like_qs[0].delete()
+  else:
+   Like.objects.create(user=request.user, post=obj)
+  return redirect("detail", slug=slug)
+ return redirect("detail", slug=slug)
