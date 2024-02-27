48ab39139fd0362ddfbf49357f5f779782a3ff66
Furkan Esen <furkanesen35@gmail.com>
2024-02-27 11:11:30 +0100

    urls done

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import register,user_login,all_users
+from rest_framework_simplejwt.views import (
+ TokenObtainPairView,
+ TokenRefreshView,
+)
 
 urlpatterns = [
  path('register/', register, name="register"),
  path('login/', user_login, name="login"),
  path('all_users/', all_users, name="all_users"),
+ path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+ path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
